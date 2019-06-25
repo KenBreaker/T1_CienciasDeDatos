b9b0bac73bfd06a291b1af73f6b8989650ebed6e
Vicente Barria <v.barriaveas@gmail.com>
2019-06-24 22:16:21 -0400

    no se nada

--- KMeans.py
+++ KMeans.py
@@ -2,6 +2,7 @@ from sklearn.cluster import KMeans
 import numpy as np
 import random
 import time
+import re
 import pandas
 import sys
 transacctions = []
@@ -10,25 +11,8 @@ kmeans_input = []
 id_max = 0
 lines = 0
 id_split = 1
-<<<<<<< HEAD
-n_lineas =2000
-n_clusters = 2
-products = {}
-n_productos=49688
-
-# Se guarda el nombre de los productos
-try:
-    file = open("INPUT/products.csv", "r", encoding='utf-8')
-    file.readline()
-    for line in file:
-        line = line.split(",")
-        products[int(line[0])] = line[1]
-    file.close()
-except FileNotFoundError:
-    print("El archivo INPUT/products.csv no existe")
-=======
-n_lineas = 3000
-n_clusters = 2
+n_lineas = 26242
+n_clusters_init = 2
 products = {}
 n_productos=39123
 
@@ -42,50 +26,28 @@ try:
     file.close()
 except FileNotFoundError:
     print("El archivo INPUT/products_train.csv no existe")
->>>>>>> caceb23e9c7f8f2b5c5a5efc85dc5db9ad361361
     exit(-1)
 
 # Transforma una lista de ID en una variable con los nombres de los productos.
 def id_to_name(id_list):
-<<<<<<< HEAD
-    id_list = str(id_list).replace("(", "").replace(")", "").split(",")
-=======
-    id_list = str(id_list).replace(" ","").strip("[]").replace("(", "").replace(")", "").replace("\n","").split(",")
->>>>>>> caceb23e9c7f8f2b5c5a5efc85dc5db9ad361361
+    id_list = str(re.sub(r'[^0-9,]', '', id_list)).split(",")
+    # id_list = str(id_list).strip("[]").replace(" ", "").replace("(", "").replace(")", "").replace("\n", "").split(",")
     name_list = []
     for i in range(0, len(id_list) - 1):
         name_list.append(products[int(id_list[i])])
     name_list = sorted(name_list, key=lambda x: x.replace(" ", ""))
     names = ""
-    if (len(name_list)):
-        names = name_list[0]
+    if len(name_list):
+        names = re.sub(r'[^a-zA-Z0-9% ]', '', name_list[0])
     for i in range(1, len(name_list)):
-        names += ", " + name_list[i]
+        if len(name_list[i]):
+            names += ", " + re.sub(r'[^a-zA-Z0-9% ]', '', name_list[i])
     return names
 
-<<<<<<< HEAD
-# Se guarda el nombre de los productos
-try:
-    file = open("INPUT/products_train.csv", "r", encoding='utf-8')
-    file.readline()
-    for line in file:
-        line = line.split(",")
-        products[int(line[0])] = line[1]
-    file.close()
-except FileNotFoundError:
-    print("El archivo INPUT/products.csv no existe")
-    exit(-1)
-
-# Read, Randomize & Split
-try:
-    print("Lectura de OUTPUT/fpgrowth_input.csv Inicializada")
-    file = open("OUTPUT/fpgrowth_input.csv", "r").readlines()
-=======
 # Read, Randomize & Split
 try:
     print("Lectura de OUTPUT/fpgrowth_train_input.csv Inicializada")
     file = open("OUTPUT/fpgrowth_train_input.csv", "r").readlines()
->>>>>>> caceb23e9c7f8f2b5c5a5efc85dc5db9ad361361
     random.shuffle(file)
     for line in file:
         if (lines==0):
@@ -98,15 +60,9 @@ try:
         lines = lines + 1
         if (lines== n_lineas):
             lines=0
-<<<<<<< HEAD
-    print("Lectura de OUTPUT/fpgrowth_input.csv finalizada")
-except FileNotFoundError:
-    print("El archivo OUTPUT/fpgrowth_input.csv no existe")
-=======
     print("Lectura de OUTPUT/fpgrowth_train_input.csv finalizada")
 except FileNotFoundError:
     print("El archivo OUTPUT/fpgrowth_train_input.csv no existe")
->>>>>>> caceb23e9c7f8f2b5c5a5efc85dc5db9ad361361
     exit(-1)
 def ClusterIndicesNumpy(clustNum, labels_array): #numpy
     return np.where(labels_array == clustNum)[0]
@@ -116,13 +72,10 @@ def ClusterIndicesComp(clustNum, labels_array): #list comprehension
 
 try:
 
-<<<<<<< HEAD
-    for n_split in range (1,2):
-=======
     for n_split in range (1,id_split):
->>>>>>> caceb23e9c7f8f2b5c5a5efc85dc5db9ad361361
         file = open("OUTPUT/Kmeans/splits/split_"+str(n_split)+".csv", "r").readlines()
         i = 0
+        n_clusters = n_clusters_init
         print("Inicializando matriz")
         kmeans_input = np.full((len(file),n_productos), '0')
         print("Matriz nula lista")
@@ -144,7 +97,7 @@ try:
             clusters_array = np.zeros([n_clusters], dtype=np.int)
             print("Ejecutando Kmeans K = "+str(n_clusters)+ " Split = "+str(n_split))
             start = time.time()
-            kmeans = KMeans(n_clusters=n_clusters,precompute_distances= True).fit(kmeans_input)
+            kmeans = KMeans(n_jobs=-1, n_clusters=n_clusters,precompute_distances=False).fit(kmeans_input)
             end = time.time()
             print("Finalizo Kmeans")
             print("Creando archivo OUTPUT")
@@ -160,21 +113,12 @@ try:
             for i in range (len(clusters_array)):
                 file_clusters.write("Cluster "+str(i)+"\n")
                 for line in ClusterIndicesComp(k, kmeans.labels_):
-<<<<<<< HEAD
-
                     file[line] = file[line].strip("[]")
-                    file_clusters.write(str(id_to_name((file[line]))))
-
-                file_clusters.write("\n")
-                k = k + 1
-=======
-                    file[line] = file[line].strip("[]")
-                    file_clusters.write(str(id_to_name(file[line])))
+                    file_clusters.write(id_to_name(file[line]) + "\n")
                     #file_clusters.write(str(file[line]))
                 file_clusters.write("\n")
                 k= k + 1
 
->>>>>>> caceb23e9c7f8f2b5c5a5efc85dc5db9ad361361
             n_clusters = n_clusters + 2
             print("OUTPUT Finalizado")
             file_clusters.close()
@@ -200,13 +144,9 @@ except FileNotFoundError:
             for i in clusters_array:
                 file_clusters.write(str(i)+"\n")
             n_clusters = n_clusters + 2
-<<<<<<< HEAD
-            print( ClusterIndicesNumpy(1, kmeans.labels_) )
-=======
 
             print( ClusterIndicesNumpy(1, kmeans.labels_) )
 
->>>>>>> caceb23e9c7f8f2b5c5a5efc85dc5db9ad361361
             print("OUTPUT Finalizado")
             file_clusters.close()
 '''
@@ -235,8 +175,4 @@ except FileNotFoundError:
                 file_clusters.write(str(i) + ",")
             file_clusters.write("\n")
         print("Archivo Creado")
-<<<<<<< HEAD
 '''
-=======
-'''
->>>>>>> caceb23e9c7f8f2b5c5a5efc85dc5db9ad361361
--- MeasureStuff.py
+++ MeasureStuff.py
@@ -1,20 +1,33 @@
 import math
+import re
 import pyfpgrowth
 
-n_split = 44                                    # Cantidad máxima de splits
-n_cluster = 20                                  # Cantidad máxima de clusters
+n_split = 1                                    # Cantidad máxima de splits
+n_cluster = 10                                  # Cantidad máxima de clusters
 entropy = []                                    # Cada posición es la entropía (de Shannon) de un split
 confidence = []                                 # Cada posición es la confianza de un split
 products = {}                                   # Colección de productos con nombre (key) y su ID
 
+
+# Escritura de la cantidad de clusters en el header
+def write_cluster_header(file_header):
+    for i in range(2, n_cluster, 2):
+        file_header.write(";" + i)
+    file_header.write("\n")
+
+
+results = open("OUTPUT/KMeans_results.csv", "w", encoding='utf-8')
+write_cluster_header(results)
+
 # Calculo de la entropía promedio de cada split
 for i in range(n_split):
     entropy.append(0.0)
+    results.write(str(i) + ";")
     for j in range(2, n_cluster + 1, 2):
         amount_of_transactions = 0
         clusters = []
         try:
-            file = open("OUTPUT/Kmeans/clusters_split" + str(i+1) + "_K_" + str(j) + ".csv", "r", encoding='utf-8')
+            file = open("OUTPUT/Kmeans/clusters/clusters_split" + str(i+1) + "_K_" + str(j) + ".csv", "r", encoding='utf-8')
             file.readline()
             for k in range(j):
                 line = file.readline()
@@ -40,19 +53,20 @@ try:
     file.readline()
     for line in file:
         line = line.split(",")
-        products[line[1]] = int(line[0])
+        product_name = re.sub(r'[^a-zA-Z0-9% ]', '', line[1]).replace("\n", "")
+        products[product_name] = int(line[0])
     file.close()
 except FileNotFoundError:
     print("El archivo INPUT/products.csv no existe")
     exit(-1)
 
 #Calculo de confianza promedio de cada split. Solo se considera mayor cluster en cada split.
-for i in range(1, n_split + 1):
+for i in range(n_split):
     confidence.append(0.0)
     for j in range(2, n_cluster + 1, 2):
         clusters = []
         try:
-            file = open("OUTPUT/Kmeans/clusters_split" + str(i + 1) + "_K_" + str(j) + ".csv", "r", encoding='utf-8')
+            file = open("OUTPUT/Kmeans/clusters/clusters_split" + str(i + 1) + "_K_" + str(j) + ".csv", "r", encoding='utf-8')
             file.readline()
             # Guarda la cantidad de transacciones en cada cluster
             for k in range(j):
@@ -61,33 +75,39 @@ for i in range(1, n_split + 1):
             index_of_max_element = clusters.index(max(clusters))
             # Lee archivo hasta encontrar sección del cluster con más transacciones
             for line in file:
-                if line == ("Cluster " + str(index_of_max_element + 1)):
+                line = line.replace("\n", "")
+                if line == str("Cluster " + str(index_of_max_element)):
                     break
             transactions = []  # Matriz de transacciones para FP-Growth
             # Transforma el nombre de cada producto a su ID y almacena el resultado
             for line in file:
-                # Si llega al siguiente cluster, finaliza
-                if line == ("Cluster " + str(index_of_max_element + 2)):
+                # Si llega al siguiente cluster,
+                line = line.replace("\n", "")
+                if line == str("Cluster " + str(index_of_max_element + 1)):
                     break
-                line = line.split(",")
-                transaction = []
-                for product in line:
-                    transaction.append(products[product])
-                transactions.append(transaction)
+                if len(line):
+                    line = line.split(", ")
+                    transaction = []
+                    for product in line:
+                        if len(product):
+                            transaction.append(products[product])
+                    transactions.append(transaction)
             file.close()
         except FileNotFoundError:
-            print("No se pudo encontrar el archivo OUTPUT/Kmeans/clusters_split" + str(i + 1) + "_K_" + str(j) + ".csv")
+            print("No se pudo encontrar el archivo OUTPUT/Kmeans/clusters/clusters_split" + str(i + 1) + "_K_" + str(j) + ".csv")
             continue
         # Se generan patrones frecuentes dado un mínimo soporte de 6
         patterns = pyfpgrowth.find_frequent_patterns(transactions, 6)
         # Se generan reglas de asociación con confianza mínima del 50%
-        rules = pyfpgrowth.generate_association_rules(patterns, 0.5)
+        rules = pyfpgrowth.generate_association_rules(patterns, 0.7)
         # Se promedia la confianza de las reglas de asociación obtenidas. VALUE[1] corresponde a la confianza
         confidence_cluster = 0.0
         for key, value in rules.items():
-            confidence[i] += float(value[1])
-        confidence[i] /= clusters[index_of_max_element]
-    confidence[i] /= n_cluster
+            confidence_cluster += float(value[1])
+            # print(str(key) + " => " + str(value[0]) + " | " + str('%.3f'%float(value[1])))
+        # print("Cantidad de reglas: " + str(len(rules)) + "\n")
+        confidence[i] += (confidence_cluster / len(rules))
+    confidence[i] /= (n_cluster / 2)
 
 for i in range(n_split):
     print("Confianza promedio split " + str(i + 1) + " = " + str('%.3f'%float(confidence[i])))
